b4ca8f1ca66492771bd7ee2cc679cd4407788d5b
zhangchao <529743595@qq.com>
2021-05-08 01:38:53 +0800

    support ts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {a} from './a.js'
 import { JSXDemo } from './jsx-demo.jsx';
 import {tsDemo} from './ts-demo.ts'
+import {TsxDemo} from './tsx-demo.tsx'
 
 console.log(tsDemo)
+console.log(TsxDemo)
 const b = import('./b.js')
 
 const hi = () => {
